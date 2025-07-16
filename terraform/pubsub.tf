4a5c5b8a812b0cf2b1d1c3cd6eb5dffad8e5043a
Morten Voss <morten@airwallet.net>
2025-07-16 22:13:10 +0200

    fix push_endpoint not double https

--- terraform/pubsub.tf
+++ terraform/pubsub.tf
@@ -21,7 +21,7 @@ resource "google_pubsub_subscription" "event_push_subs" {
   topic = google_pubsub_topic.events[each.key].id
 
   push_config {
-    push_endpoint = "https://${google_cloud_run_v2_service.main.uri}${each.value}"
+    push_endpoint = "${google_cloud_run_v2_service.main.uri}${each.value}"
 
     oidc_token {
       service_account_email = google_service_account.cloud_run.email
