f166047dbef88579ac4a596ae30d17fdaae591b5
Morten Voss <morten@airwallet.net>
2025-07-16 21:10:09 +0200

    do canary testing

--- deploy.sh
+++ deploy.sh
@@ -14,10 +14,30 @@ TERRAFORM_DIR="./terraform"
 # FETCH STABLE REVISION
 # ----------------------
 echo "Fetching current stable Cloud Run revision..."
-STABLE_REVISION=$(gcloud run services describe "$SERVICE_NAME" \
+
+
+
+ACTIVE_REVISIONS=$(gcloud run services describe "$SERVICE_NAME" \
   --project="$PROJECT_ID" \
   --region="$REGION" \
-  --format="value(status.traffic[0].revisionName)")
+  --format="value(status.traffic)")
+  
+
+if [[ -z "$ACTIVE_REVISIONS" ]]; then
+echo "❌ Failed to fetch active revisions. Is the service deployed yet?"
+exit 1
+fi
+
+echo "✅ Active revisions: $ACTIVE_REVISIONS"
+
+ACTIVE_REVISIONS_JSON=$(echo "$ACTIVE_REVISIONS" \
+  | sed "s/'/\"/g" \
+  | sed "s/};{/},{/g" \
+  | sed "s/True/true/g" \
+  | awk '{print "[" $0 "]"}')
+
+# Extract the revisionName with highest percent
+STABLE_REVISION=$(echo "$ACTIVE_REVISIONS_JSON" | jq -r 'max_by(.percent).revisionName')
 
 if [[ -z "$STABLE_REVISION" ]]; then
   echo "❌ Failed to fetch stable revision. Is the service deployed yet?"
