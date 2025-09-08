bbe7ba5eb8fe0309a788934fa0ac5ff01838f56f
Morten Voss <morten@airwallet.net>
2025-09-08 21:38:07 +0200

    fixed server type

--- terraform/hetzner/main.tf
+++ terraform/hetzner/main.tf
@@ -25,7 +25,7 @@ resource "hcloud_ssh_key" "default" {
 resource "hcloud_server" "tribble" {
   name        = var.server_name
   image       = "ubuntu-22.04"
-  server_type = "cpx22"  # 2 vCPU, 4GB RAM, €3.29/month
+  server_type = "cx22"   # 2 vCPU, 4GB RAM, €3.79/month
   location    = "fsn1"   # Falkenstein, Germany (closest to Denmark)
 
   ssh_keys = [hcloud_ssh_key.default.id]
